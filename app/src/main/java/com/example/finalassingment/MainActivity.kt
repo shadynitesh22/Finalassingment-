cc3fb6d0cb47d6e399c6f6f7a9d739293d458063
nitesh <nitesh.poudel@hotmail.com>
2021-01-18 00:38:13 +0545

    ok

--- app/src/main/java/com/example/finalassingment/MainActivity.kt
+++ app/src/main/java/com/example/finalassingment/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.finalassingment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var owner:Button
+    private lateinit var renter:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        owner=findViewById(R.id.owner)
+        renter=findViewById(R.id.renter)
+        owner.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container,Owner())
+                addToBackStack(null)
+                commit()
+
+            }
+            renter.setOnClickListener {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.container,Renter())
+                    addToBackStack(null)
+                    commit()
+                }
+            }
+        }
     }
 }
