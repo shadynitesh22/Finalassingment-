7606146a2ed8a4a9eb26fb32025322fd46a00b73
nitesh <nitesh.poudel@hotmail.com>
2021-01-19 15:05:56 +0545

    ok

--- app/src/main/java/com/example/finalassingment/Splashscreen2.kt
+++ app/src/main/java/com/example/finalassingment/Splashscreen2.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-class SplashScreenActivity : AppCompatActivity() {
+class Splashscreen2 : AppCompatActivity() {
     private val splastime:Long=5000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_splashscreen2)
         Handler().postDelayed(
             {
-                startActivity(Intent(this,Splashscreen2::class.java))
+                startActivity(Intent(this,MainActivity::class.java))
                 finish()
             },splastime)
     }
