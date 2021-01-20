f7f0b2aa22ac68993e0e9119950e2bb81585ffee
nitesh <nitesh.poudel@hotmail.com>
2021-01-20 16:47:23 +0545

    DashBoard Design

--- app/src/main/java/com/example/finalassingment/SplashScreenActivity.kt
+++ app/src/main/java/com/example/finalassingment/SplashScreenActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
-    private val splastime:Long=5000
+    private val splastime:Long=3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
--- app/src/main/java/com/example/finalassingment/Splashscreen2.kt
+++ app/src/main/java/com/example/finalassingment/Splashscreen2.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 
 class Splashscreen2 : AppCompatActivity() {
-    private val splastime:Long=5000
+    private val splastime:Long=3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen2)
