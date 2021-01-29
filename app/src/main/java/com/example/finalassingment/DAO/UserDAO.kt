29f103ac8e561de57c3b94f16e4949d7b6fd00f4
nitesh <nitesh.poudel@hotmail.com>
2021-01-29 19:15:21 +0545

    Room Database Created

--- app/src/main/java/com/example/finalassingment/DAO/UserDAO.kt
+++ app/src/main/java/com/example/finalassingment/DAO/UserDAO.kt
@@ -1,5 +1,17 @@
 package com.example.finalassingment.DAO
 
-interface UserDAO {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import com.example.finalassingment.Model.User
 
+@Dao
+interface UserDAO {
+    @Insert
+    suspend fun registerUser(User: User)
+    @Update
+    suspend fun updateUser(User: User)
+    @Delete
+    suspend fun deleteUser(User: User)
 }
---
+++ app/src/main/java/com/example/finalassingment/DB/UserDb.kt
@@ -0,0 +1,37 @@
+package com.example.finalassingment.DB
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.finalassingment.DAO.UserDAO
+import com.example.finalassingment.Model.User
+
+@Database(
+    entities = [(User::class)],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class UserDb:RoomDatabase() {
+    abstract fun getUserDAO(): UserDAO
+    companion object {
+        @Volatile
+        private var instance : UserDb? = null
+        fun getInstance(context : Context) : UserDb{
+            if(instance == null){
+                synchronized(UserDb::class){
+                    instance = buildDatabase(context)
+                }
+            }
+            return instance!!
+        }
+        fun buildDatabase(context : Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                UserDb::class.java,
+                "Userdb"
+            ).build()
+    }
+
+}
