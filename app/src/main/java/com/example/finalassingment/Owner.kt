7eea47df9e8d6b56727780cb6bee75389578943d
nitesh <nitesh.poudel@hotmail.com>
2021-01-29 18:43:46 +0545

    Intent in Fragments

--- app/src/main/java/com/example/finalassingment/Owner.kt
+++ app/src/main/java/com/example/finalassingment/Owner.kt
@@ -1,57 +1,41 @@
 package com.example.finalassingment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log.i
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Owner.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Owner : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var username:EditText
+    private lateinit var pass:EditText
+    private lateinit var login:Button
+    private lateinit var signup:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+           }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_owner, container, false)
+        val view=inflater.inflate(R.layout.fragment_owner, container, false)
+                username=view.findViewById(R.id.usernames)
+                pass=view.findViewById(R.id.passwords)
+                login=view.findViewById(R.id.logins)
+                signup=view.findViewById(R.id.signups)
+        signup.setOnClickListener {
+            val intent = Intent (getActivity(),OwnerSignUpActivity::class.java)
+            getActivity()?.startActivity(intent)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Owner.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                Owner().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
+
 }
--- app/src/main/java/com/example/finalassingment/Renter.kt
+++ app/src/main/java/com/example/finalassingment/Renter.kt
@@ -1,57 +1,38 @@
 package com.example.finalassingment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Renter.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Renter : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var username: EditText
+    private lateinit var pass: EditText
+    private lateinit var login: Button
+    private lateinit var signup: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_renter, container, false)
+
+                val view= inflater.inflate(R.layout.fragment_renter, container, false)
+        username=view.findViewById(R.id.usernames1)
+        pass=view.findViewById(R.id.passwords1)
+        login=view.findViewById(R.id.logins1)
+        signup=view.findViewById(R.id.signups1)
+        signup.setOnClickListener {
+            val intent = Intent (getActivity(),RenterSignUpActivity::class.java)
+            getActivity()?.startActivity(intent)
+                    }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Renter.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                Renter().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
 }
