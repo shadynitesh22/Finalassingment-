0d9e92f5780f10a4af4bfe7c5d4ab36b05ebee2d
nitesh <nitesh.poudel@hotmail.com>
2021-01-29 17:42:48 +0545

    UI Update

--- app/src/main/java/com/example/finalassingment/SplashScreenActivity.kt
+++ app/src/main/java/com/example/finalassingment/SplashScreenActivity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageButton
 
 class SplashScreenActivity : AppCompatActivity() {
-    private val splastime:Long=3000
+    private lateinit var next:ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed(
-            {
-                startActivity(Intent(this,Splashscreen2::class.java))
-                finish()
-            },splastime)
-    }
+        next=findViewById(R.id.next)
+        next.setOnClickListener {
+            startActivity(Intent(this,Splashscreen2::class.java))
+        }
+
+
+
+
+}
 }
