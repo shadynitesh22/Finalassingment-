bdd2a720c6334faf224f7b9f9d0476b93da10211
nitesh <nitesh.poudel@hotmail.com>
2021-01-29 19:22:05 +0545

    Permissions Added

--- app/src/main/java/com/example/finalassingment/Dashbord1Activity.kt
+++ app/src/main/java/com/example/finalassingment/Dashbord1Activity.kt
@@ -1,11 +1,41 @@
 package com.example.finalassingment
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class Dashbord1Activity : AppCompatActivity() {
+    private val permissions = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashbord1)
+        if (!hasPermission()) {
+            requestPermission()
+        }
+
+    }
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@Dashbord1Activity,
+            permissions, 1
+        )
+    }
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
     }
 }
