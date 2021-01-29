b8ffcc6fd3ae2efc871f63fcbacce83ed88a208d
nitesh <nitesh.poudel@hotmail.com>
2021-01-29 17:52:51 +0545

    Signup Added

---
+++ app/src/main/java/com/example/finalassingment/OwnerSignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalassingment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OwnerSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owner_sign_up)
+    }
+}
---
+++ app/src/main/java/com/example/finalassingment/RenterSignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalassingment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RenterSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_renter_sign_up)
+    }
+}
