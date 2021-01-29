e2df6c9ba9727858f32d8014d8d021bedb76f443
nitesh <nitesh.poudel@hotmail.com>
2021-01-29 19:42:50 +0545

    Sign up Data Saved in Room Database

--- app/src/main/java/com/example/finalassingment/Model/User.kt
+++ app/src/main/java/com/example/finalassingment/Model/User.kt
@@ -1,14 +1,15 @@
 package com.example.finalassingment.Model
 
+import android.widget.EditText
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 @Entity
-data class User (
-    val username:String?=null,
-    val email:String?=null,
-    val phone:String?=null,
-    val pass:String?=null,
-    val conf:String?=null
+data class User(
+    val username: String? =null,
+    val email: String? =null,
+    val phone: String? =null,
+    val pass: String? =null,
+    val conf: String? =null
 
         ){
         @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/example/finalassingment/RenterSignUpActivity.kt
+++ app/src/main/java/com/example/finalassingment/RenterSignUpActivity.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.finalassingment.DB.UserDb
+import com.example.finalassingment.Model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RenterSignUpActivity : AppCompatActivity() {
     private lateinit var username: EditText
@@ -26,6 +32,28 @@ class RenterSignUpActivity : AppCompatActivity() {
         login=findViewById(R.id.login1)
         login.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
+        }
+        signup.setOnClickListener {
+            val usernamez=username.text.toString()
+            val emailz=email.text.toString()
+            val phonez=phone.text.toString()
+            val passz=pass.text.toString()
+            val cpassz=cpass.text.toString()
+            if (passz !=cpassz){
+                pass.error="Passwords Don't Match"
+                pass.requestFocus()
+                return@setOnClickListener
+            }else{
+                val user= User(usernamez,emailz,phonez,passz,cpassz)
+                CoroutineScope(Dispatchers.IO).launch {
+                    //StudentDB(this@RegisterUserActivity).getUserDAO().registerUser(user)
+                    UserDb.getInstance(this@RenterSignUpActivity).getUserDAO().registerUser(user)
+                }
+                Toast.makeText(this, "User registered", Toast.LENGTH_SHORT).show()
+
+            }
+
+
         }
     }
 }
